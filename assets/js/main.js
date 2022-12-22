7da5b2566053ed1f2164f6099a9d562173f29d03
MIKO SETIAWAN <98937432+mikosetiawan@users.noreply.github.com>
2022-12-22 13:31:30 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let start = document.getElementById('start');
+let meteroid = document.getElementById('meteroid');
+let rocket = document.getElementById('rocket');
+let text = document.getElementById('text');
+let button = document.getElementById('button');
+
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    start.style.left = value * 0.25 + 'px';
+    rocket.style.top = value * -0.5 + 'px';
+    text.style.marginBottom = value * 1 + 'px';
+    button.style.marginBottom = value * 1 + 'px';
+
+})
